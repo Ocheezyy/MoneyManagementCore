6d93955ef557315d6cfb8ec4871d7df9ea466663
Sean O'Donnell <sodonnel@student.highlands.edu>
2020-06-05 16:34:47 -0400

    Changed DB info and fields accessed by EF

--- EFDataAccessLibrary/Migrations/20200605201502_inial.Designer.cs
+++ EFDataAccessLibrary/Migrations/20200605201502_inial.Designer.cs
@@ -4,14 +4,16 @@ using EFDataAccessLibrary.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFDataAccessLibrary.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200605201502_inial")]
+    partial class inial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFDataAccessLibrary/Migrations/20200605201502_inial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDataAccessLibrary.Migrations
+{
+    public partial class inial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dccus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Transaction_Number = table.Column<string>(maxLength: 200, nullable: true),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    Memo = table.Column<string>(maxLength: 200, nullable: true),
+                    Amount_Debit = table.Column<string>(maxLength: 20, nullable: true),
+                    Balance = table.Column<double>(nullable: false),
+                    Fees = table.Column<string>(maxLength: 30, nullable: true),
+                    Principal = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dccus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Discs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Trans_Date = table.Column<DateTime>(nullable: false),
+                    Post_Date = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 100, nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    Category = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dccus");
+
+            migrationBuilder.DropTable(
+                name: "Discs");
+        }
+    }
+}
